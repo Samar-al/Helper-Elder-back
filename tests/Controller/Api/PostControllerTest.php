506e2c2de5be4d7bca4c552e36108ab3e1fa42c0
Samar-al <samar.alkhalilchebaye@gmail.com>
2023-03-08 17:04:36 +0100

    test

--- tests/Controller/Api/PostControllerTest.php
+++ tests/Controller/Api/PostControllerTest.php
@@ -2,15 +2,46 @@
 
 namespace App\Tests\Controller\Api;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class PostControllerTest extends ApiTestCase
+class PostControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testEditPost(): void
     {
-        $response = static::createClient()->request('GET', '/');
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('ufournier@fouquet.com');
+
+         // simulate $testUser being logged in
+         $client->loginUser($testUser);
+         $id = 552;
+         $crawler = $client->request('POST', '/api/annonce/'.$id.'/modifier');
+
+       // $post = $this->createPost($testUser); 
+
+        $data = [
+            'title' => 'Updated post title',
+            'content' => 'Updated post content',
+        ];
+        $json = json_encode($data);
+        
+        $client->request('POST', '/api/annonce/'.$post->getId().'/modifier', [], [], [], $json);
+
+        // Check if the response is successful
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+
+        // Check if the post data has been updated
+        $entityManager = $this->getEntityManager();
+        $entityManager->refresh($post);
+        $this->assertSame($data['title'], $post->getTitle());
+        $this->assertSame($data['content'], $post->getContent());
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonContains(['@id' => '/']);
+        $this->assertSelectorTextContains('h1', 'Hello World');
     }
 }
